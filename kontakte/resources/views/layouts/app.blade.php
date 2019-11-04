f8484439e39f9ceedb67179dc4383613b1b8b713
Moritz <moritz.banhardt@web.de>
2019-11-04 17:29:33 +0100

    Auslagerung von der linken Spalte in eigenes Template

--- kontakte/resources/views/layouts/app.blade.php
+++ kontakte/resources/views/layouts/app.blade.php
@@ -78,26 +78,8 @@
                 <div class="row">
                     <div class="col-md-2">
                         <div class="list-group">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="row">
-                                        <div class="col-auto mr-auto">
-                                            <p class="headline">Kontaktliste</p>
-                                        </div>
-                                        <div class="col-auto">
-                                            <p>+</p>
-                                        </div>
-                                    </div>
-                                    <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-                                    <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-                                    <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-                                </div>
-                            
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div class="col">
+                         @include ('layouts.kontaktliste')
+                         <div class="col">
                         <div class="content">
                             @yield('content')
                         </div>
---
+++ kontakte/resources/views/layouts/kontaktliste.blade.php
@@ -0,0 +1,19 @@
+
+<div class="row">
+    <div class="col-12">
+        <div class="row">
+            <div class="col-auto mr-auto">
+                <p class="headline">Kontaktliste</p>
+            </div>
+            <div class="col-auto">
+                <p>+</p>
+            </div>
+            </div>
+                <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+                <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+            </div>
+    
+        </div>
+    </div>
+</div>
