9db8a72b5fe23bf9e299d489e98a43b509940cee
Moritz <moritz.banhardt@web.de>
2019-11-13 18:11:27 +0100

    Links auf der Welcome Seite geändert

--- kontakte/resources/views/welcome.blade.php
+++ kontakte/resources/views/welcome.blade.php
@@ -6,13 +6,10 @@
 </div>
 
 <div class="links">
-    <a href="https://laravel.com/docs">Docs</a>
-    <a href="https://laracasts.com">Laracasts</a>
-    <a href="https://laravel-news.com">News</a>
-    <a href="https://blog.laravel.com">Blog</a>
-    <a href="https://nova.laravel.com">Nova</a>
-    <a href="https://forge.laravel.com">Forge</a>
-    <a href="https://www.youtube.com/watch?v=aULDpjQHjCw&list=PLIBtxqFY3QHqJCIZbbOWx0lJYW2PbbrB0">Benutztes Laravel Tutorial</a>
+    <a href="https://www.youtube.com/watch?v=OcZEbwxdg4Q&t=17s">Docker-Tutorial</a>
+    <a href="https://www.youtube.com/watch?v=ImtZ5yENzgE">Benutztes Laravel Tutorial</a>
+    <a href="https://www.youtube.com/watch?v=EVqoQgdfQYQ">Laravel Installations Tutorial</a>
+    <a href="https://www.youtube.com/watch?v=aULDpjQHjCw&list=PLIBtxqFY3QHqJCIZbbOWx0lJYW2PbbrB0">Noch ein benutztes Laravel Tutorial</a>
     <a href="https://github.com/moritzb98">GitHub</a>
 </div>
 @endsection
